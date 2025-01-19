1e093a44ae2553aa65b879f767dece3c54e2ee20
ryuyakamioka <r.kamioka@gmail.com>
2025-01-19 15:51:13 +0900

    springbootのプロジェクトを追加

---
+++ backend/src/main/java/com/kamiokaweb/redo/RedoApplication.java
@@ -0,0 +1,13 @@
+package com.kamiokaweb.redo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kamiokaweb/redo/RedoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamiokaweb.redo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
