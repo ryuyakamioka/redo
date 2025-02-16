8a013c5fdeaee91f39f6c40f8346d3d14def9e5c
ryuyakamioka <r.kamioka@gmail.com>
2025-02-16 22:43:40 +0900

    デプロイテスト

--- frontend/pages/hello.vue
+++ frontend/pages/hello.vue
@@ -1,5 +1,28 @@
 <template>
   <div>
     <h1>hello world!!</h1>
+    <div>現在時刻: {{ currentTime }}</div>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const currentTime = ref('')
+
+const updateCurrentTime = () => {
+  const now = new Date()
+  currentTime.value = now.toLocaleString()
+}
+
+onMounted(() => {
+  // コンポーネントがマウントされたら、現在時刻を更新する関数を呼び出す
+  updateCurrentTime()
+
+  // 1秒ごとに現在時刻を更新する
+  const intervalId = setInterval(updateCurrentTime, 1000)
+
+  // コンポーネントがアンマウントされたら、インターバルをクリアする
+  onUnmounted(() => clearInterval(intervalId))
+})
+</script>
